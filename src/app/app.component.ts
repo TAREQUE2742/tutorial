29f49f56be31db925660e8ab241e4ce671ac08c0
tareque2742 <tareque2742@gmail.com>
2022-04-21 05:55:46 +0600

    4/21/20222

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tutorial';
+  title = 'hello from AppComponent';
+
+  getMain(){
+    return`hello`;
+  }
 }
+
